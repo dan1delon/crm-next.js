2ea12dc8bf7ec7918f456ff2e2a9d5a104110e56
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-12-09 13:44:41 +0200

    tailwind.css

---
+++ src/app/components/status-label.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export enum Status {
+  Active = 'active',
+  NotActive = 'notActive',
+  Pending = 'pending',
+  Suspended = 'suspended',
+}
+
+export interface StatusLabelProps {
+  children: React.ReactNode;
+  status: Status;
+}
+
+export default function StatusLabel({ children, status }: StatusLabelProps) {
+  return (
+    <div
+      className={`inline-flex items-center py-1 px-3.5 text-sm font-medium rounded-3xl ${
+        (status === Status.Active && 'bg-green-100 text-green-700') ||
+        (status === Status.NotActive && 'bg-red-100 text-red-700') ||
+        (status === Status.Pending && 'bg-orange-100 text-orange-700') ||
+        (status === Status.Suspended && 'bg-blue-100 text-blue-700') ||
+        ''
+      }
+      }}`}
+    >
+      <div className="w-1 h-1 mr-2 bg-current rounded-full" />
+      {children}
+    </div>
+  );
+}
